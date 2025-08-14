31d3062ec4f39a767b3473f7841ece08a071aa29
anas2001abdullah <anasabdullah388@hotmail.com>
2025-08-14 16:05:58 +0300

    Updating homepage placeholder

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -284,10 +284,18 @@ const HomePage = ({ user, duration = 15000 }) => {
   };
   if (coursesLoading || usersLoading)
     return (
+  <>
+  <div className="bg-light dark:bg-slate-800">
+    
+      <HeroBanner />
+
+     
+      <AboutSection />
+      </div>
       <div
         role="status"
         className="space-y-8 animate-pulse md:space-y-0 md:space-x-8 rtl:space-x-reverse md:flex md:items-center px-56 py-16 bg-light dark:bg-slate-800"
-      >
+        >
         <div className="flex items-center justify-center w-full h-48 bg-gray-300 rounded sm:w-96 dark:bg-slate-600">
           <svg
             className="w-10 h-10 text-gray-500 dark:text-gray-600"
@@ -295,7 +303,7 @@ const HomePage = ({ user, duration = 15000 }) => {
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
             viewBox="0 0 20 18"
-          >
+            >
             <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
           </svg>
         </div>
@@ -309,6 +317,7 @@ const HomePage = ({ user, duration = 15000 }) => {
         </div>
         <span className="sr-only">Loading...</span>
       </div>
+            </>
     );
 
   const topLikedCourses = courses.sort((a, b) => b.likes - a.likes).slice(0, 5);
